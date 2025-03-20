d269dec13d3cf42a63061d2c89547be59ac307de
Dmitri Prikhodko <dmitri_pr@rambler.ru>
2025-03-20 21:20:41 +1000

    Minor changes

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -49,7 +49,7 @@ SECRET_KEY = getenv(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = getenv('DJANGO_DEBUG', '0') == 1
+DEBUG = getenv('DJANGO_DEBUG', '1') == 1
 
 ALLOWED_HOSTS = [
     '0.0.0.0',
